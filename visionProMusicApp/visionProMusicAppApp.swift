c73d2a1c4d1d6b883632756fda44cd701ce2c7b0
Antonin SIMON <antonin.simon@epsi.fr>
2024-05-15 18:37:17 +0200

    Initial Commit

---
+++ visionProMusicApp/visionProMusicAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  visionProMusicAppApp.swift
+//  visionProMusicApp
+//
+//  Created by Antonin Simon on 15/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct visionProMusicAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
